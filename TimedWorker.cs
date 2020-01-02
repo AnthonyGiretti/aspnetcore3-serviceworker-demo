e680d155c4caebd05444e4a035084d421f489f57
Anthony Giretti <anthony.giretti@gmail.com>
2020-01-02 10:34:20 -0500

    update project with counter

---
+++ TimedWorker.cs
@@ -0,0 +1,55 @@
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+namespace DemoWorkerService
+{
+    public class TimedWorker : IHostedService, IDisposable
+    {
+        private readonly ILogger<TimedWorker> _logger;
+        private Timer _timer;
+        private static object _lock = new object();
+        private int _counter = 0;
+
+        public TimedWorker(ILogger<TimedWorker> logger)
+        {
+            _logger = logger;
+        }
+
+        public Task StartAsync(CancellationToken cancellationToken)
+        {
+            _timer = new Timer(MyJob, null, TimeSpan.Zero, TimeSpan.FromSeconds(3));
+            return Task.CompletedTask;
+        }
+
+        public Task StopAsync(CancellationToken cancellationToken)
+        {
+            return Task.CompletedTask;
+        }
+
+        public void Dispose()
+        {
+
+        }
+
+        private void MyJob(object state)
+        {
+            _logger.LogDebug($"Try to execute iteration {_counter + 1} of MyJob ");
+            if (Monitor.TryEnter(_lock))
+            {
+                try
+                {
+                    _logger.LogDebug($"Running MyJob iteration {_counter}");
+                    Thread.Sleep(8000);
+                }
+                finally
+                {
+                    _counter++;
+                    Monitor.Exit(_lock);
+                }
+            }
+        }
+    }
+}
